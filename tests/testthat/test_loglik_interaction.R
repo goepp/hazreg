8b6d324effacdee44587c1df57897cbcf05aa7c1
Vivien Goepp <vivien.goepp@gmail.com>
2018-03-19 14:38:23 +0100

    minor formal change

--- tests/testthat/test_loglik_interaction.R
+++ tests/testthat/test_loglik_interaction.R
@@ -12,8 +12,10 @@ test_that("O, R, and pen must be positive", {
 test_that("dimensions of par, O, and R must agree", {
   expect_error(
     loglik_interaction(rnorm(100), matrix(1, 10, 10), matrix(1, 10, 15), 0),
-    'Error: dimensions of O, R, and pen must agree')
+    'Error: dimensions of O, R, and pen must agree'
+    )
   expect_error(
     loglik_interaction(rnorm(100), matrix(1, 10, 10), matrix(1, 10, 15), 0),
-    'Error: dimensions of O, R, and pen must agree')
+    'Error: dimensions of O, R, and pen must agree'
+    )
 })
