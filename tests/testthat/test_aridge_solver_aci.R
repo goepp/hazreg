387429e439d2798d6cb077ce4e3875c5a286e3be
Vivien Goepp <vivien.goepp@gmail.com>
2018-03-27 11:18:48 +0200

    create test of aridge_solver_aci

---
+++ tests/testthat/test_aridge_solver_aci.R
@@ -0,0 +1,32 @@
+context("aridge_solver_aci.R")
+
+library(lattice)
+library(tidyverse)
+library(reshape2)
+library(limSolve)
+library(igraph)
+library(bandsolve)
+
+set.seed(0)
+K <- 3
+J <- 4
+O <- matrix(rpois(K * J, 1), K, J)
+R <- matrix(rpois(K * J, 10), K, J)
+par <- rnorm(K * J)
+
+
+test_that("aridge_solver_aci yiels same result with and withou use_band", {
+  pen_vect <- 1
+  mat <- aridge_solver_aci(O, R, pen_vect, sample_size = 10, use_band = FALSE)
+  rot <- aridge_solver_aci(O, R, pen_vect, sample_size = 10, use_band = TRUE)
+  expect_equal(mat, rot)
+  skip_on_cran()
+  pen_vect <- 10 ^ (-2:3)
+  mat <- aridge_solver_aci(O, R, pen_vect, sample_size = 10, use_band = FALSE)
+  rot <- aridge_solver_aci(O, R, pen_vect, sample_size = 10, use_band = TRUE)
+  expect_equal(mat, rot)
+})
+
+
+
+
